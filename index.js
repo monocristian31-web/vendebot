9961315156bcc87b30b6230387bf528b7a0921d5
Alex <monocristian31@gmail.com>
2026-03-03 14:25:16 -0500

    Fix PWA Safari

--- index.js
+++ index.js
@@ -1209,11 +1209,8 @@ app.get('/sw.js', (req, res) => {
   res.setHeader('Content-Type', 'application/javascript');
   res.send(`
 self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => { return; });
   `);
 });
 
